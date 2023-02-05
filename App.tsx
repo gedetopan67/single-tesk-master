4d2baa500cb2479dd92073322ceca8a5044ea8d6
gedetopan67 <102726989+gedetopan67@users.noreply.github.com>
2023-02-06 00:59:05 +0800

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import NavBar from "./molecules/NavBar";
+// import { useBookStore } from "./store";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
